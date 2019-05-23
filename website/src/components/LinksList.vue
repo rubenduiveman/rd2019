1de7fc65eebbffc052d4b83da75504d0ee2e20d0
Ruben Duiveman <ruben@uptrends.com>
2019-05-23 12:21:19 +0200

    Implemented links list component on links page.

---
+++ website/src/components/LinksList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li>
+      <a class='outbound'
+        href="https://medium.com/@jasonyuan/the-desktop-metaphor-must-die-676fbb34afdb"
+      >The Desktop Metaphor Must Die</a>
+      <label class="date">May 23, 2019</label>
+      <p class="desc">Thought-provoking article on how the desktop metafore is reaching its due date. Focuses on how human-focused a desktop really is, since it allows us to get overwhelmed by multitasking, and how it should stimulate us to get in <a href="https://en.wikipedia.org/wiki/Flow_(psychology)">a state of flow</a>.</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LinksList extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+    @import "@/style/theme.scss";
+
+    p a {
+        @include color("neutral-900");
+        text-decoration: underline;
+        font-weight: normal;
+
+        &:hover {
+            @include color("primary-500");
+        }
+    }
+</style>
--- website/src/views/Links.vue
+++ website/src/views/Links.vue
@@ -3,16 +3,19 @@
     <p class="hero">
         What follows is a list of interesting links and worthwile articles that I've come across. You might find them interesting, too.
     </p>
+    <LinksList/>
   </Section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import LinksList from "@/components/LinksList.vue";
 import Section from "@/components/Section.vue";
 
 @Component({
   components: {
+      LinksList,
       Section
   }
 })
