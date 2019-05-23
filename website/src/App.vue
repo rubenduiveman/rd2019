29c10320b7bec718f0348c0b58fc8e9c69f74bad
Ruben Duiveman <ruben@uptrends.com>
2019-05-23 14:34:17 +0200

    Sped the page change transition up.

--- website/src/App.vue
+++ website/src/App.vue
@@ -44,7 +44,7 @@ export default class App extends Vue {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition-duration: 0.2s;
+  transition-duration: 0.1s;
   transition-property: opacity;
   transition-timing-function: ease;
 }
