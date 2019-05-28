8cc431dce6a1692d93b211ae15f0fedb1ed093b2
Ruben Duiveman <ruben@uptrends.com>
2019-05-28 11:11:27 +0200

    Load links via the API.

--- website/src/components/LinksList.vue
+++ website/src/components/LinksList.vue
@@ -1,37 +1,9 @@
 <template>
   <ul>
-    <li>
-      <a
-        class="outbound"
-        href="https://www.forbes.com/sites/tracybrower/2019/05/27/agile-why-your-efforts-will-fail-and-what-to-do-about-it/"
-      >Agile: why your efforts will fail and what to do about it</a>
-      <label class="date">May 28, 2019</label>
-      <p
-        class="desc"
-      >This is an article on common pitfalls Agile development teams might come across. There are no real solutions offered in this piece, but naming issues is the first step towards solving them.</p>
-    </li>
-    <li>
-      <a
-        class="outbound"
-        href="https://medium.com/@jasonyuan/the-desktop-metaphor-must-die-676fbb34afdb"
-      >The Desktop Metaphor Must Die</a>
-      <label class="date">May 23, 2019</label>
-      <p class="desc">
-        Thought-provoking article on how the desktop metafore is reaching its due date. Focuses on how human-focused a desktop really is, since it allows us to get overwhelmed by multitasking, and how it should stimulate us to get in
-        <a
-          href="https://en.wikipedia.org/wiki/Flow_(psychology)"
-        >a state of flow</a>.
-      </p>
-    </li>
-    <li>
-      <a class="outbound" href="https://palettte.app/">Palette App</a>
-      <label class="date">Feb 10, 2019</label>
-      <p class="desc">
-        Color palette generator that actually feels well thought-out. This tool enables you to create consistent and logical color palettes that work. Also super fun to visualize the
-        <a
-          href="https://en.wikipedia.org/wiki/HSL_and_HSV"
-        >HSL</a> relationship between the colors in a palette.
-      </p>
+    <li v-for="linkdata in links" :key="linkdata.url">
+      <a class="outbound" :href="linkdata.url">{{ linkdata.title }}</a>
+      <label class="date">{{ linkdata.date }}</label>
+      <p class="desc" v-html="linkdata.description"></p>
     </li>
   </ul>
 </template>
@@ -39,8 +11,17 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import * as Api from "@/logic/api";
+import { LinkData } from "@/logic/models";
+
 @Component
-export default class LinksList extends Vue {}
+export default class LinksList extends Vue {
+  private links: LinkData[] = [];
+
+  public async beforeMount() {
+    this.links = await Api.getLinks();
+  }
+}
 </script>
 
 <style lang="scss" scoped>
