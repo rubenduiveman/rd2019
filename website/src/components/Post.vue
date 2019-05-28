22344301869d01680aaa24bd1daf862edea5c3c3
Ruben Duiveman <ruben@uptrends.com>
2019-05-28 21:24:45 +0200

    Implemented a proper post footer.

--- website/src/components/Post.vue
+++ website/src/components/Post.vue
@@ -2,7 +2,11 @@
   <article v-if="post">
     <PostHeader :data="post"/>
     <main v-html="post.content"></main>
-    <aside>liked this post? discuss on twitter</aside>
+    <aside>
+      Want to react?
+      <a :href="shareLink" target="_blank">Discuss this post on twitter</a> or
+      <a href="https://twitter.com/rubenduiveman">tweet to me</a>.
+    </aside>
   </article>
 </template>
 
@@ -18,12 +22,17 @@ import { PostData } from "../logic/models";
 export default class Post extends Vue {
   @Prop() private id!: string;
 
-  // TODO: Post footer
-
   private postData: PostData | null = null;
 
   private get post() {
     return this.postData;
+  }
+
+  private get shareLink() {
+    const postName = encodeURIComponent(this.post!.title);
+    const postLink = encodeURIComponent(this.post!.shareUrl);
+    // tslint:disable-next-line:max-line-length
+    return `https://twitter.com/intent/tweet?text=I%20just%20read%20%22${postName}%22%20by%20%40rubenduiveman&url=${postLink}`;
   }
 
   public async beforeMount() {
@@ -63,4 +72,17 @@ main {
   padding-top: 24px;
 }
 
+.theme-dark aside a,
+.theme-light aside a {
+  background-image: none;
+  margin: 0;
+}
+
+aside {
+  border-top: 1px solid;
+  @include border-color("neutral-500");
+  margin-top: 2em;
+  padding-top: 0.5em;
+  width: 50%;
+}
 </style>
